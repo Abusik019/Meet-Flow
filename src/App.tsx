b5e72fd0acc6e177dd5557b3e73df5c6cd759e61
Abusik019 <abusroyale@gmail.com>
2025-03-04 11:43:15 +0300

    created project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2 className='font-medium text-2xl text-[red]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. At inventore iusto laboriosam commodi aliquid iste corporis deleniti! Laudantium at id corrupti, alias ratione facere vitae voluptatibus nisi dolorum est aut!</h2>
+    </>
+  )
+}
+
+export default App
